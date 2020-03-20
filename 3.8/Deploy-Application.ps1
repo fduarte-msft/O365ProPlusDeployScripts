802d06c8f411fa5a67def1a25beccb2c1bb5f0c2
Fred Duarte <fred.duarte@microsoft.com>
2020-03-19 22:45:01 -0600

    Deploy-Application
    Added pre-install routines

--- 3.8/Deploy-Application.ps1
+++ 3.8/Deploy-Application.ps1
@@ -50,7 +50,10 @@ Param (
 	[Parameter(Mandatory=$false)]
 	[switch]$TerminalServerMode = $false,
 	[Parameter(Mandatory=$false)]
-	[switch]$DisableLogging = $false
+	[switch]$DisableLogging = $false,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet('O365ProPlusRetail','VisioProRetail','ProjectProRetail','ProjectProXVolume','VisioProXVolume','ProjectStdXVolume','VisioStdXVolume')]
+	[string]$Products = 'O365ProPlusRetail'
 )
 
 Try {
@@ -61,15 +64,15 @@ Try {
 	##* VARIABLE DECLARATION
 	##*===============================================
 	## Variables: Application
-	[string]$appVendor = ''
-	[string]$appName = ''
-	[string]$appVersion = ''
-	[string]$appArch = ''
+	[string]$appVendor = 'Microsoft'
+	[string]$appName = 'Office 365 ProPlus'
+	[string]$appVersion = '16.0'
+	[string]$appArch = 'x86'
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
 	[string]$appScriptVersion = '1.0.0'
 	[string]$appScriptDate = '23/09/2019'
-	[string]$appScriptAuthor = '<author name>'
+	[string]$appScriptAuthor = 'Fred Duarte'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
 	[string]$installName = ''
@@ -116,14 +119,290 @@ Try {
 		##*===============================================
 		[string]$installPhase = 'Pre-Installation'
 
-		## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
-		Show-InstallationWelcome -CloseApps 'iexplore' -AllowDefer -DeferTimes 3 -CheckDiskSpace -PersistPrompt
+		## Show Welcome Message, close Office applications if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
+		Show-InstallationWelcome -CloseApps 'excel,groove,onenote,onenotem,infopath,onenote,outlook,mspub,powerpnt,lync,communicator,winword,winproj,visio' -AllowDefer -DeferTimes 3 -CheckDiskSpace -PersistPrompt
 
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
 
-		## <Perform Pre-Installation tasks here>
+		## Create Products list
+		[Collections.Generic.List[String]]$newProducts = $Products
 
+		## Get existing configuration
+		$c2rConfigRegPath = 'HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration'
+
+		## If Office 365 ProPlus products are installed, inventory which ones
+		if (Test-Path -Path $c2rConfigRegPath -ErrorAction SilentlyContinue) {
+			# Get C2R architecture
+			$c2rArch = Get-ItemProperty -Path $c2rConfigRegPath -Name 'Platform'
+			if ($c2rArch.Platform -match 'x64') {
+				Write-Log -Message "Office 365 ProPlus will be migrated to 32-bit" -Severity 1 -Source $deployAppScriptFriendlyName
+				$office365 = $true
+			}
+		
+			# Get C2R channel
+			$c2rChannel = Get-ItemProperty -Path $c2rConfigRegPath -Name 'CDNBaseURL'
+			if ($c2rChannel.CDNBaseUrl -notmatch 'http://officecdn.microsoft.com/pr/7ffbc6bf-bc32-4f92-8982-f9dd17fd3114') {
+				Write-Log -Message "Office 365 ProPlus will be migrate to Semi-Annual Channel." -Severity 1 -Source $deployAppScriptFriendlyName
+				$migrateChannel = $true
+			}
+		
+			# Get C2R installed products
+			$c2rInstalledProducts = Get-ItemProperty -Path $c2rConfigRegPath -Name 'ProductReleaseIds'
+		}
+		
+		# Build a list of products to install
+		if ($c2rInstalledProducts) {
+			# Multiple C2R products installed
+			if ($c2rInstalledProducts.ProductReleaseIds -match ',') {
+				$c2rProducts = $c2rInstalledProducts.ProductReleaseIds.Split(',')
+				foreach ($c2rProduct in $c2rProducts) {
+					if ($c2rProduct -notmatch $Products) {
+						$newProducts.Add($c2rProduct)
+					}
+				}
+			# Single C2R product installed
+			} else {
+				if ($c2rInstalledProducts.ProductReleaseIds -notmatch $Products) {
+					$newProducts.Add($c2rInstalledProducts.ProductReleaseIds)
+		
+				}
+			}
+		} 
+		
+		# Convert Visio Standard 2016 to Visio Pro for Office 365
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioStdXVolume') {
+			if ($Products -match 'VisioProRetail') {
+				if ($newProducts.Contains('VisioStdXVolume')) {
+					$newProducts.Remove('VisioStdXVolume')
+					Write-Log -Message "Microsoft Visio Standard 2016 (VL) will be migrated to Microsoft Visio Pro for Office 365 (Subscription)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Visio Professional 2016 to Visio Pro for Office 365
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioProXVolume') {
+			if ($Products -match 'VisioProRetail') {
+				if ($newProducts.Contains('VisioProXVolume')) {
+					$newProducts.Remove('VisioProXVolume')
+					Write-Log -Message "Microsoft Visio Proffesional 2016 (VL) will be migrated to Microsoft Visio Pro for Office 365 (Subscription)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Project Standard 2016 to Project Pro for Office 365
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'ProjectStdXVolume') {
+			if ($Products -match 'ProjectProRetail') {
+				if ($newProducts.Contains('ProjectStdXVolume')) {
+					$newProducts.Remove('ProjectStdXVolume')
+					Write-Log -Message "Microsoft Project Standard 2016 (VL) will be migrated to Microsoft Project Pro for Office 365 (Subscription)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Project Professional 2016 to Project Pro for Office 365
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'ProjectProXVolume') {
+			if ($Products -match 'ProjectProRetail') {
+				if ($newProducts.Contains('ProjectProXVolume')) {
+					$newProducts.Remove('ProjectProXVolume')
+					Write-Log -Message "Microsoft Project Proffesional 2016 (VL) will be migrated to Microsoft Project Pro for Office 365 (Subscription)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Visio Pro for Office 365 to Visio Standard 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioProRetail') {
+			if ($Products -match 'VisioStdXVolume') {
+				if ($newProducts.Contains('VisioProRetail')) {
+					$newProducts.Remove('VisioProRetail')
+					Write-Log -Message "Microsoft Visio Pro for Office 365 (Subscription) will be migrated to Microsoft Visio Standard 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Visio Pro for Office 365 to Visio Professional 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioProRetail') {
+			if ($Products -match 'VisioProXVolume') {
+				if ($newProducts.Contains('VisioProRetail')) {
+					$newProducts.Remove('VisioProRetail')
+					Write-Log -Message "Microsoft Visio Pro for Office 365 (Subscription) will be migrated to Microsoft Visio Professional 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Project Pro for Office 365 to Project Standard 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'ProjectProRetail') {
+			if ($Products -match 'ProjectStdXVolume') {
+				if ($newProducts.Contains('ProjectProRetail')) {
+					$newProducts.Remove('ProjectProRetail')
+					Write-Log -Message "Microsoft Project Pro for Office 365 (Subscription) will be migrated to Microsoft Project Standard 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Project Pro for Office 365 to Project Professional 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'ProjectProRetail') {
+			if ($Products -match 'ProjectProXVolume') {
+				if ($newProducts.Contains('ProjectProRetail')) {
+					$newProducts.Remove('ProjectProRetail')
+					Write-Log -Message "Microsoft Project Pro for Office 365 (Subscription) will be migrated to Microsoft Project Professional 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Visio Standard 2016 to Visio Professional 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioStdXVolume') {
+			if ($Products -match 'VisioProXVolume') {
+				if ($newProducts.Contains('VisioStdXVolume')) {
+					$newProducts.Remove('VisioStdXVolume')
+					Write-Log -Message "Microsoft Visio Standard 2016 (VL) will be migrated to Microsoft Visio Professional 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+		
+		# Convert Visio Professional 2016 to Visio Standard 2016
+		if ($c2rInstalledProducts.ProductReleaseIds -match 'VisioProXVolume') {
+			if ($Products -match 'VisioStdXVolume') {
+				if ($newProducts.Contains('VisioProXVolume')) {
+					$newProducts.Remove('VisioProXVolume')
+					Write-Log -Message "Microsoft Visio Professional 2016 (VL) will be migrated to Microsoft Visio Standard 2016 (VL)" -Severity 1 -Source $deployAppScriptFriendlyName
+					$office365 = $true
+				}
+			}
+		}
+
+		## Log Office 365 ProPlus inventory data
+		$oldConfig = $c2rInstalledProducts.ProductReleaseIds -join ','
+		$newConfig = $newProducts -join ','
+
+		## Log old configuration and new configuration
+		Write-Log -Message "Old Configuraiton: $oldConfig" -Severity 1 -Source $deployAppScriptFriendlyName
+		Write-Log -Message "New Configuration: $newConfig" -Severity 1 -Source $deployAppScriptFriendlyName
+
+		## Get all Microsoft installations
+		$officeProducts = Get-InstalledApplication -Name 'Microsoft'
+
+		## Determine which Office products we will remove 
+		foreach ($officeProduct in $officeProducts) {
+			if ($officeProduct.DisplayName -match 'office professional plus 2007') {
+				Write-Log -Message "$($officeProduct.DisplayName) is installed. Will be removed to install Office 365 ProPlus (Semi-Annual Channel)." -Severity 1 -Source $deployAppScriptFriendlyName
+				$office2k7 = $true
+			}
+
+			if ($officeProduct.DisplayName -match 'office professional plus 2010') {
+				Write-Log -Message "$($officeProduct.DisplayName) is installed. Will be removed to install Office 365 ProPlus (Semi-Annual Channel)." -Severity 1 -Source $deployAppScriptFriendlyName
+				$office2k10 = $true
+			}
+
+			if ($officeProduct.DisplayName -match 'office professional plus 2013') {
+				Write-Log -Message "$($officeProduct.DisplayName) is installed. Will be removed to install Office 365 ProPlus (Semi-Annual Channel)." -Severity 1 -Source $deployAppScriptFriendlyName
+				$office2k13 = $true
+			}
+
+			if ($officeProduct.DisplayName -match 'office professional plus 2016') {
+				Write-Log -Message "$($officeProduct.DisplayName) is installed. Will be removed to install Office 365 ProPlus (Semi-Annual Channel)." -Severity 1 -Source $deployAppScriptFriendlyName
+				$office2k16 = $true
+			}
+
+			if ($officeProduct.DisplayName -match 'office professional plus 2019') {
+				Write-Log -Message "$($officeProduct.DisplayName) is installed. Will be removed to install Office 365 ProPlus (Semi-Annual Channel)." -Severity 1 -Source $deployAppScriptFriendlyName
+				$office2k19 = $true
+			}
+		}
+
+		## Scrub previous versions of Office
+		
+		## Office 2007
+		if ($office2k7) {
+			try {
+				Write-Log -Message "Uninstalling Office Professional Plus 2007" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Execute-Process -Path "$envSystem32Directory\cscript.exe" -Parameters "$dirSupportFiles\OffScrub07.vbs ALL /FR /QUIET /LOG $configToolkitLogDir\Office_2007_Uninstall" -WindowStyle 'Hidden'
+				Write-Log -Message "Uninstalled Office Professional PLus 2007. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# Office 2010
+		if ($office2k10) {
+			try {
+				Write-Log -Message "Uninstalling Office Professional Plus 2010" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrub10.vbs ALL /FR /QUIET /LOG $configToolkitLogDir\Office_2010_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled Office Professional PLus 2010. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# Office 2013
+		if ($office2k13) {
+			try {
+				Write-Log -Message "Uninstalling Office Professional Plus 2013" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrub_O15msi.vbs ALL /FR /QUIET /LOG $configToolkitLogDir\Office_2013_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled Office Professional Plus 2013. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# infopath 2013
+		if ($info2k13) {
+			try {
+				Write-Log -Message "Uninstalling Microsoft InfoPath 2013" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrub_O15msi.vbs ALL /FR /QUIET /LOG $configToolkitLogDir\Office_2013_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled InfoPath 2013. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# Office 2016
+		if ($office2k16) {
+			try {
+				Write-Log -Message "Uninstalling Office Professional Plus 2016" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrub_O16msi.vbs ALL /FR /QUIET /LOG $configToolkitLogDir\Office_2016_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled Office Professional Plus 2016. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# Office 2019
+		if ($office2k19) {
+			try {
+				Write-Log -Message "Uninstalling Office Professional Plus 2019" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrubC2R.vbs /QUIET /RETERRORSUCCESS /LOG $configToolkitLogDir\Office_proplus_2019_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled Office Professional Plus 2019. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
+
+		# Office 365 ProPlus
+		if ($office365 -or $migrateChannel) {
+			try {
+				Write-Log -Message "Uninstalling Office 365 ProPlus" -Severity 1 -Source $deployAppScriptFriendlyName
+				$scrub = Start-Process -FilePath 'cscript' -ArgumentList "$dirSupportFiles\OffScrubC2R.vbs /QUIET /RETERRORSUCCESS /LOG $configToolkitLogDir\Office_365_proplus_Uninstall" -WindowStyle Hidden -Wait -PassThru -ErrorAction Stop
+				$scrub.WaitForExit()
+				Write-Log -Message "Uninstalled Office 365 ProPlus. Exit code: $($scrub.ExitCode)" -Severity 1 -Source $deployAppScriptFriendlyName
+			} catch {
+				Write-Log -Message "Failure: $($Error[0].Exception.Message)" -Severity 3 -Source $deployAppScriptFriendlyName
+			}
+		}
 
 		##*===============================================
 		##* INSTALLATION
@@ -147,7 +426,7 @@ Try {
 		## <Perform Post-Installation tasks here>
 
 		## Display a message at the end of the install
-		If (-not $useDefaultMsi) { Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait }
+		If (-not $useDefaultMsi) { Show-InstallationPrompt -Message 'Office 365 ProPlus has been configured.' -ButtonRightText 'OK' -Icon Information -NoWait }
 	}
 	ElseIf ($deploymentType -ieq 'Uninstall')
 	{
